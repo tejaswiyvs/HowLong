6b67fd928b185841ca9323abcbf59ec1d2d115a9
Tejaswi Yerukalapudi <tejaswi.yvs@gmail.com>
2012-09-14 16:34:44 -0400

    Support for standard shortcuts

---
+++ HowLong/TYHowLongApp.h
@@ -0,0 +1,15 @@
+//
+//  TYHowLongApp.h
+//  HowLong
+//
+//  Created by Tejaswi Yerukalapudi on 9/14/12.
+//  Copyright (c) 2012 Tejaswi Yerukalapudi. All rights reserved.
+//
+
+// Subclassed NSApplication to support various standard shortcuts in the application while acting as a LSUIElement.
+
+#import <Cocoa/Cocoa.h>
+
+@interface TYHowLongApp : NSApplication
+
+@end
